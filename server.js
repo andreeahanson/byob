577119eeb73af8712031e61929b3f155501ebc3c
Andreea Hanson <andreea.hanson7@gmail.com>
2019-08-14 14:45:10 -0600

    Pseudocode end points with paths

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-//GET ALL DOCTORS
-//GET ALL PATIENTS
+//GET ALL DOCTORS ('/clinique/doctors')
+//GET ALL PATIENTS OF A CERTAIN DOCTOR???('/clinique/doctors/1/patients')
 
-//POST PATIENT
-//PUT PATIENT
-//DELETE PATIENT
+//POST PATIENT('/clinique/doctors/2/patients')
+//PUT PATIENT ('/clinique/doctors/2/patients/1004')
+//DELETE PATIENT ('/clinique/doctors/2/patiens/1004')
