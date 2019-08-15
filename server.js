d46cc7d6fb5b42e1703bfb7164c0538659f95dfd
Andreea Hanson <andreea.hanson7@gmail.com>
2019-08-15 10:19:20 -0600

    Modify getDoctors handler &add get doctor by id endpoint and handler

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const doctorsData = require('./data/doctors-data.js')
+const patientsData = require('./data/patients-data.js')
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = "Clinique";
@@ -9,18 +13,39 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 })
 
-app.locals.doctors = [];
-app.locals.doctors = doctorsData;
-
+//GET ALL DOCTORS ('/clinique/doctors')
 app.get('/clinique/doctors', (request, response) => {
-  const doctors = app.locals.doctors;
-
-  return response.json({ doctors })
+  database('doctors').select()
+    .then((doctors) => {
+      response.status(200).json(doctors);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    })
 })
 
-//GET ALL DOCTORS ('/clinique/doctors')
-//GET ALL PATIENTS OF A CERTAIN DOCTOR???('/clinique/doctors/1/patients')
+//GET ONE DOCTOR ('/clinique/doctors/:id')
+app.get('/clinique/doctors/:id', (request, response) => {
+  database('doctors').where('id', request.params.id).select()
+  .then(doctors => {
+    if(doctors.length) {
+      response.status(200).json(doctors);
+    } else {
+      response.status(404).json({
+        error: `Could not find doctor with id ${request.params.id}`
+      })
+    }
+  })
+  .catch(error => {
+    response.status(500).json({ error });
+  })
+})
 
-//POST PATIENT('/clinique/doctors/2/patients')
-//PUT PATIENT ('/clinique/doctors/2/patients/1004')
-//DELETE PATIENT ('/clinique/doctors/2/patiens/1004')
+
+//GET ALL PATIENTS OF A CERTAIN DOCTOR???('/clinique/doctors/:id/patients')
+
+
+
+//POST PATIENT('/clinique/doctors/:id/patients')
+//PUT PATIENT ('/clinique/doctors/:id/patients/1004')
+//DELETE PATIENT ('/clinique/doctors/:id/patiens/1004')
